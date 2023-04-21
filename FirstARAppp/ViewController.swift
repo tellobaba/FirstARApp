49a0e3cf278a9f0bf1fcf2f931dfd4bad4327b78
tellobaba <olatunjitello17@gmail.com>
2023-04-21 15:01:14 +0100

    Initial Commit

--- FirstARAppp/ViewController.swift
+++ FirstARAppp/ViewController.swift
@@ -7,18 +7,67 @@
 
 import UIKit
 import RealityKit
+import ARKit
 
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        arView.session.delegate = self
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        setUpARView()
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.addGestureRecognizer(UITapGestureRecognizer(
+            target: self,
+            action: #selector(handleTap(recognizer:))))
+    }
+    
+    // Setup Methods
+    
+    func setUpARView(){
+        arView.automaticallyConfigureSession = false
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal, .vertical]
+        configuration.environmentTexturing = .automatic
+        arView.session.run(configuration)
+    }
+    
+    // Object Placement
+    
+    @objc func handleTap(recognizer: UITapGestureRecognizer){
+        let location = recognizer.location(in: arView)
+        
+        let results = arView.raycast(from: location,
+                                     allowing: .estimatedPlane,
+                                     alignment: .horizontal)
+        
+        if let firstResult = results.first {
+            let anchor = ARAnchor(name: "argun", transform: firstResult.worldTransform)
+            arView.session.add(anchor: anchor)
+        } else {
+            print("Object placement failed, couldn't find surface")
+        }
+    }
+    func placeObject(named entityName: String, for anchor: ARAnchor){
+        let entity = try! ModelEntity.loadModel(named: entityName)
+        
+        entity.generateCollisionShapes(recursive: true)
+        arView.installGestures([.rotation, .translation], for: entity)
+        
+        let anchorEntity = AnchorEntity(anchor: anchor)
+        anchorEntity.addChild(entity)
+        arView.scene.addAnchor(anchorEntity)
+    }
+}
+
+extension ViewController: ARSessionDelegate {
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        for anchor in anchors {
+            if let anchorName = anchor.name, anchorName == "argun" {
+                placeObject(named: anchorName, for: anchor)
+            }
+        }
     }
 }
