c7b8e5493c7be701a720a2f433ff362ad93b835d
tellobaba <olatunjitello17@gmail.com>
2023-04-21 09:57:01 +0100

    Initial Commit

---
+++ FirstARAppp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FirstARAppp
+//
+//  Created by Oluwatomiwa on 21/04/2023.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
